69b2f2b4f7b4b2041d48f17334583322db436023
rcarrasco9000 <rcarrasco9000@gmail.com>
2023-05-23 13:58:58 -0400

    Initial version

---
+++ product-service/src/main/java/com/cubibuild/productservice/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.cubibuild.productservice.controller;
+
+import com.cubibuild.productservice.dto.ProductRequest;
+import com.cubibuild.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+
+
+}
---
+++ product-service/src/main/java/com/cubibuild/productservice/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.cubibuild.productservice.service;
+
+import com.cubibuild.productservice.dto.ProductRequest;
+import com.cubibuild.productservice.model.Product;
+import com.cubibuild.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+       Product product = Product.builder()
+               .name(productRequest.getName())
+               .description(productRequest.getDescription())
+               .price(productRequest.getPrice())
+               .build();
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+}
